1337b9d017111721ffcb9bafb4a67c1475240503
TheyGoPoof <bwood1178@gmail.com>
2025-03-30 16:36:10 -0400

    1 on 1 connection done

---
+++ client.py
@@ -0,0 +1,24 @@
+import socket
+import sys
+import time
+
+server_socket = socket.socket()
+hostname = socket.gethostname()
+server_ip = socket.gethostbyname(hostname)
+port = 8080 # doesn't have to be 8080, but it's standard
+
+print("This is your ip address: ", server_ip)
+server_host = input("Enter your friend's ip address: ")
+name = input("Enter your name: ")
+
+server_socket.connect((server_host, port))
+
+server_socket.send(name.encode())
+server_name = (server_socket.recv(1024)).decode()
+print(server_name + " has connected.")
+
+while True:
+    message = (server_socket.recv(1024)).decode()
+    print(server_name + ": " + message)
+    message = input("Me: ")
+    server_socket.send(message.encode())
--- server.py
+++ server.py
@@ -6,3 +6,23 @@ server_socket = socket.socket()
 hostname = socket.gethostname()
 server_ip = socket.gethostbyname(hostname)
 port = 8080 # doesn't have to be 8080, but it's standard
+
+server_socket.bind((hostname, port))
+print("Successfully bound server to port ", port)
+print("Your IP is: ", server_ip)
+
+name = input("Enter your name: ")
+server_socket.listen(1)
+
+connection, address = server_socket.accept()
+print("Connected to: ", address[0])
+
+client = (connection.recv(1024)).decode()
+print(client + " has connected.")
+connection.send(name.encode())
+
+while True:
+    message = input("Me: ")
+    connection.send(message.encode())
+    message = (connection.recv(1024)).decode()
+    print(client + ": " + message)
