37e1fcb15fcca3284e2eaeffc5a70e257035fd76
TheyGoPoof <bwood1178@gmail.com>
2025-03-30 17:03:58 -0400

    multi user and multi messaging working server side

--- client.py
+++ client.py
@@ -1,22 +1,45 @@
 import socket
+import threading
+
+def receive_messages(server_socket):
+    while True:
+        try:
+            message = (server_socket.recv(1024)).decode()
+            if not message:
+                print("Connection closed by the server.")
+                break
+            print(message)
+        except ConnectionResetError:
+            print("Connection lost.")
+            break
+
+def send_messages(server_socket):
+    while True:
+        try:
+            message = input("Me: ")
+            server_socket.send(message.encode())
+        except ConnectionResetError:
+            print("Connection lost.")
+            break
 
 server_socket = socket.socket()
 hostname = socket.gethostname()
 server_ip = socket.gethostbyname(hostname)
-port = 8080 # doesn't have to be 8080, but it's standard
+port = 8080
 
-print("This is your ip address: ", server_ip)
-server_host = input("Enter your friend's ip address: ")
+print("This is your IP address: ", server_ip)
+server_host = input("Enter your friend's IP address: ")
 name = input("Enter your name: ")
 
 server_socket.connect((server_host, port))
-
 server_socket.send(name.encode())
 server_name = (server_socket.recv(1024)).decode()
 print(server_name + " has connected.")
 
-while True:
-    message = (server_socket.recv(1024)).decode()
-    print(server_name + ": " + message)
-    message = input("Me: ")
-    server_socket.send(message.encode())
+receive_thread = threading.Thread(target=receive_messages, args=(server_socket,))
+send_thread = threading.Thread(target=send_messages, args=(server_socket,))
+receive_thread.start()
+send_thread.start()
+receive_thread.join()
+send_thread.join()
+server_socket.close()
--- server.py
+++ server.py
@@ -1,26 +1,60 @@
 import socket
+import threading
 
+# List to store active client connections
+clients = []
+
+def broadcast(message, sender_connection):
+    """Send a message to all clients except the sender."""
+    for client in clients:
+        if client != sender_connection:
+            try:
+                client.send(message)
+            except:
+                # Remove the client if sending fails
+                clients.remove(client)
+
+def handle_client(connection, address):
+    """Handle communication with a single client."""
+    print(f"Connected to: {address[0]}")
+    try:
+        client_name = connection.recv(1024).decode()
+        print(f"{client_name} has joined the chat.")
+        broadcast(f"{client_name} has joined the chat.".encode(), connection)
+        clients.append(connection)
+
+        while True:
+            try:
+                message = connection.recv(1024)
+                if not message:
+                    break
+                print(f"{client_name}: {message.decode()}")
+                broadcast(f"{client_name}: {message.decode()}".encode(), connection)
+            except ConnectionResetError:
+                break
+    finally:
+        # Remove the client from the list and notify others
+        if connection in clients:
+            clients.remove(connection)
+        print(f"{client_name} has left the chat.")
+        broadcast(f"{client_name} has left the chat.".encode(), connection)
+        connection.close()
+
+# Server setup
 server_socket = socket.socket()
 hostname = socket.gethostname()
 server_ip = socket.gethostbyname(hostname)
-port = 8080 # doesn't have to be 8080, but it's standard
+port = 8080
 
 server_socket.bind((hostname, port))
 print("Successfully bound server to port ", port)
 print("Your IP is: ", server_ip)
 
-name = input("Enter your name: ")
-server_socket.listen(1)
+server_socket.listen(5)  # Allow up to 5 pending connections
+print("Waiting for connections...")
 
-connection, address = server_socket.accept()
-print("Connected to: ", address[0])
-
-client = (connection.recv(1024)).decode()
-print(client + " has connected.")
-connection.send(name.encode())
-
+# Accept clients dynamically
 while True:
-    message = input("Me: ")
-    connection.send(message.encode())
-    message = (connection.recv(1024)).decode()
-    print(client + ": " + message)
+    connection, address = server_socket.accept()
+    client_thread = threading.Thread(target=handle_client, args=(connection, address))
+    client_thread.start()
